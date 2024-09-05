ea4b721ee85c20c6cb8d3f7dd92fa15d3d5aea22
Oliver Tacke <o.tacke@posteo.de>
2024-09-05 15:08:00 +0200

    Fix task detection

--- src/scripts/models/content-instance.js
+++ src/scripts/models/content-instance.js
@@ -159,7 +159,7 @@ export default class ContentInstance {
       return false;
     }
 
-    if (instance.isTask) {
+    if (typeof instance.isTask === 'boolean') {
       return instance.isTask; // Content will determine if it's task on its own
     }
 
